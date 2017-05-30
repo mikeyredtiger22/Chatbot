6933af19105747e42de2fd94ad4d51d28000186b
Michael P-F <mikeyredtiger22@gmail.com>
2017-05-30 12:13:29 +0100

    Bot conversation Functionality and code cleanup

--- Client.py
+++ Client.py
@@ -1,42 +1,38 @@
-import Bot1
-import Bot2
 import requests
 import json
 
 
 def main():
-    bot1 = Bot1
-    bot2 = Bot2
 
     print ("Connected to client service")
     while True:
         user_input = raw_input("Type 'list' to see all bots or 'connect <Bot Name>' to be connected:\n")
         if user_input.startswith("list"):
             print "Available bots:\n" \
-                  "Bot1\n" \
-                  "Bot2\n"
+                  "\tBot1\n" \
+                  "\tBot2\n"
         elif user_input.startswith('connect'):
             if user_input.split(" ")[1] == "Bot1":
-
-                user_input = raw_input("You are now connected with Bot1\n")
-                print "User Input: ", user_input
-
-                json_user_input = json.dumps({'message': user_input})
-                print "Json user input: ", json_user_input
-
-                api_response = requests.post('http://localhost:5000/', json=json_user_input)
-                print api_response, "\n"
-
+                conversation('http://localhost:5000/')
+                print "You have disconnected from Bot1"
                 continue
-            elif user_input.split(" ")[1] == "Bot1":
-                print "You are now connected with Bot2\n"
+            elif user_input.split(" ")[1] == "Bot2":
+                conversation('http://localhost:5001/')
+                print "You have disconnected from Bot2"
                 continue
             else:
                 print "You have not selected an available bot.\n"
                 continue
+        else:
+            print "You have not entered a valid instruction.\n"
 
-    # ChatBot.get_response(bot1, input_item)
 
+def conversation(bot_url):
+    user_input = raw_input("You are now connected with Bot1. Type 'exit' to disconnect\n")
+    while not user_input.startswith("exit"):
+        json_user_input = json.dumps({'message': user_input})
+        api_response = str(requests.post(bot_url, json=json_user_input)) + "\n"
+        user_input = raw_input(api_response)
 
 if __name__ == '__main__':
     main()
